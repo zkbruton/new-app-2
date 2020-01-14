122ffe6c51b3f2d2468aee3842b3bac8234c8fc9
zkbruton <59748866+zkbruton@users.noreply.github.com>
2020-01-14 14:18:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,17 @@ import dash_html_components as html
 import os
 from dash.dependencies import Input, Output
 
-zika = pd.read_csv('https://raw.githubusercontent.com/chrisalbon/simulated_datasets/master/data.xlsx',
-                 sep='\t'
-                  )
+df_spt=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/df_spt.csv',index_col=0)
+repair_data=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/repair_data.csv',index_col=0)
+repair_data_copy=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/repair_data_copy.csv',index_col=0)
+nb_data=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/nb_data.csv',index_col=0)
+nb_data_copy=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/nb_data_copy.csv',index_col=0)
+con_data=pd.read_csv('https://raw.githubusercontent.com/zkbruton/csv-files/master/con_data.csv',index_col=0)
+df_spt['Date']=pd.to_datetime(df_spt['Date'])
+repair_data['Date']=pd.to_datetime(repair_data['Date'])
+repair_data_copy['Date']=pd.to_datetime(repair_data_copy['Date'])
+nb_data['Date']=pd.to_datetime(nb_data['Date'])
+nb_data_copy['Date']=pd.to_datetime(nb_data_copy['Date'])
 
 app = dash.Dash(__name__)
 server = app.server
@@ -20,7 +28,7 @@ server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-        html.H2('Zika Explorer')
+        html.H2('Testing Grounds')
 ])
 
 if __name__ == '__main__':
