737303e80dc0bb596aad2492d19d505c04d8ffa2
zkbruton <59748866+zkbruton@users.noreply.github.com>
2020-01-14 20:02:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import plotly.plotly as plotly
 #import plotly.plotly as py
-#import plotly.graph_objs as go
+import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
 #import datetime
